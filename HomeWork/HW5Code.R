3bbb8cc4d95f36d8cee2151cc92e4308f92901a9
unknown <dlaroche@GYF9Q32.htgmolecular.com>
2015-10-12 10:28:37 -0700

    Completed first part of project 1

--- HomeWork/HW5Code.R
+++ HomeWork/HW5Code.R
@@ -3,7 +3,9 @@ library(ggplot2)
 df <- read.csv("HomeWork/SEERdata.csv")
 df <- df[-c(1,3),]
 names(df)[1] <- "Age"
+df$All.races <- gsub(",", "", df$All.races)
 df$All.races <- as.numeric(as.character(df$All.races))
 
- ggplot(df, aes(x=Age, y=All.races)) + geom_point() + geom_line()
+ ggplot(df, aes(x=Age, y=All.races, group=1)) + geom_point() + geom_line() + ylab("Incidence of All Cancer Types") + theme(axis.text.x=element_text(angle=90, hjust=1))
+ ggsave("./HomeWork/HW5_plot.png", width=5, height=5)
  
